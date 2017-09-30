bb490b7f60b367d49644506f7c36669687e1a3c5
Mariana Sickler <marianaleal_93@hotmail.com>
2017-09-30 14:18:32 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -33,4 +33,12 @@ def self.find_by_name(name)
   self.all.detect{|song| song.name == name }
 end
 
+def self.find_or_create_by_name(name)
+  if self.find_by_name(name) == nil 
+    self.create_by_name(name)
+  else self.all 
+  end
+    
+end
+
 end
